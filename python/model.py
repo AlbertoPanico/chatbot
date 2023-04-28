8178fda101a0eee33571c49754704097e9985216
AlbertoPanico <panicoalberto99@gmail.com>
2023-04-28 22:39:29 +0200

    train_nonva

--- python/model.py
+++ python/model.py
@@ -6,8 +6,8 @@ class NeuralNetwork(nn.Module):
     def __init__(self, input_size, hidden_size, num_classes):
         super(NeuralNetwork, self).__init__()
         self.l1 = nn.Linear(input_size, hidden_size)
-        self.l2 = nn.Linear(input_size, hidden_size)
-        self.le = nn.Linear(input_size, num_classes)
+        self.l2 = nn.Linear(hidden_size, hidden_size)
+        self.le = nn.Linear(hidden_size, num_classes)
         self.relu = nn.ReLU()
 
     def forward(self, x):
