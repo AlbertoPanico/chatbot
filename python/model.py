ecbeb4c303abebc94892ddc777c6d84ee7c37a06
AlbertoPanico <panicoalberto99@gmail.com>
2023-04-28 22:38:26 +0200

    errore_train.py

---
+++ python/model.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+
+# let's create our neural network
+class NeuralNetwork(nn.Module):
+    def __init__(self, input_size, hidden_size, num_classes):
+        super(NeuralNetwork, self).__init__()
+        self.l1 = nn.Linear(input_size, hidden_size)
+        self.l2 = nn.Linear(input_size, hidden_size)
+        self.le = nn.Linear(input_size, num_classes)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        output = self.l1(x)
+        output = self.relu(output)
+        output = self.l2(output)
+        output = self.relu(output)
+        output = self.l3(output)
+        # no activation and no softmax
+        return output
